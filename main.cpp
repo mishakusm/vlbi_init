b8cd3c6211ec3150632a0ae19c48660886adef0f
mishakusm <160011550+mishakusm@users.noreply.github.com>
2024-03-22 11:30:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -261,7 +261,9 @@ int main (int arc, char **argv)
       			        fprintf(stderr, "-i, -d and -s options are required.\n");
       				return 5;
    			}
-			char* nm_interface = "netmap:";
+			char* nm_interface;
+			nm_interface=(char*) malloc (sizeof(char)*30);
+			nm_interface = "netmap:"
 			strcat(nm_interface, interface);
    			struct nm_desc *nmd;
    			nmd = nm_open(nm_interface, nullptr, NM_OPEN_NO_MMAP, nullptr);
