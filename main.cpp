2a2d4a80204c076bc0e665e4eae1c2483e8a7ac8
mishakusm <160011550+mishakusm@users.noreply.github.com>
2024-04-18 11:59:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -212,7 +212,7 @@ int main (int arc, char **argv)
 	{
 		//printf("%d\n", pkt->VDIF_Data_Frame.body[k]);
 	}
-	set_pkt_header(pkt);
+	set_pkt_header(pkt, rate);
 	for (int i = 0; i <= 8; i++)
 	{
 		printf("%d\n", pkt->VDIF_Data_Frame.vdif_header[i].to_int);
@@ -255,12 +255,15 @@ int main (int arc, char **argv)
         				return 4;
 				}
 			break;
+			
 			case 't':
 				char *time_str = optarg;
 			    	time = (clock_t)atol(time_str);
 			break;
-			case 't':
-				rate = optarg;
+			
+			case 'r':
+				char *rate_str = optarg;
+				rate = (int)atol(rate_str);
 			break;
 			
 		}
